9ff5be73060e45506ea6f05b97d250011944b377
Andrei Shevkaliuk <sedemaqu@gmail.com>
2024-11-01 12:07:20 +0300

    Moved flags to internal

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -4,17 +4,18 @@ import (
 	"context"
 
 	"github.com/madatsci/gophermart/internal/app"
+	"github.com/madatsci/gophermart/internal/app/flags"
 )
 
 func main() {
-	if err := parseFlags(); err != nil {
+	if err := flags.Parse(); err != nil {
 		panic(err)
 	}
 
 	app, err := app.New(context.Background(), app.Options{
-		RunAddress:           runAddress,
-		AccrualSystemAddress: accrualSystemAddress,
-		DatabaseURI:          databaseURI,
+		RunAddress:           flags.RunAddress,
+		AccrualSystemAddress: flags.AccrualSystemAddress,
+		DatabaseURI:          flags.DatabaseURI,
 	})
 	if err != nil {
 		panic(err)
