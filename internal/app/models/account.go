d838d7af310605dd9721286a4001f6b263bc1d4b
Andrei Shevkaliuk <sedemaqu@gmail.com>
2024-11-04 16:15:11 +0300

    Changed field data types in Account model

--- internal/app/models/account.go
+++ internal/app/models/account.go
@@ -1,14 +1,18 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
 
 type Account struct {
-	ID                 string    `bun:",pk,type:uuid"`
-	UserID             string    `bun:",unique,notnull"`
-	CurrentPointsTotal int64     `bun:",notnull,default:0"`
-	WithdrawnTotal     int64     `bun:",notnull,default:0"`
-	CreatedAt          time.Time `bun:",notnull,default:current_timestamp"`
-	UpdatedAt          time.Time `bun:",notnull,default:current_timestamp"`
+	ID                 string          `bun:",pk,type:uuid"`
+	UserID             string          `bun:",unique,notnull"`
+	CurrentPointsTotal decimal.Decimal `bun:",notnull,default:0"`
+	WithdrawnTotal     decimal.Decimal `bun:",notnull,default:0"`
+	CreatedAt          time.Time       `bun:",notnull,default:current_timestamp"`
+	UpdatedAt          time.Time       `bun:",notnull,default:current_timestamp"`
 
 	User   User     `bun:"rel:belongs-to,join:user_id=id"`
 	Orders []*Order `bun:"rel:has-many,join:id=account_id"`
