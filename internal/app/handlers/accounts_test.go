8d409d92f6b8701c4541d744992cfaa9786c17ae
Andrei Shevkaliuk <sedemaqu@gmail.com>
2024-11-21 20:06:45 +0300

    Added negative test cases for GetBalance handler

--- internal/app/handlers/accounts_test.go
+++ internal/app/handlers/accounts_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/madatsci/gophermart/internal/app/models"
 	"github.com/madatsci/gophermart/internal/app/server/middleware"
 	"github.com/madatsci/gophermart/internal/app/store/database/mocks"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -51,5 +52,35 @@ func TestGetBalanceHandler(t *testing.T) {
 		require.NoError(t, err)
 
 		assert.Equal(t, `{"current":500,"withdrawn":1000}`+"\n", string(respStr), "unexpected response body")
+	})
+
+	t.Run("unauthorized user", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, path, http.NoBody)
+		require.NoError(t, err)
+
+		r := httptest.NewRecorder()
+
+		h.GetBalance(r, req)
+		resp := r.Result()
+		defer resp.Body.Close()
+
+		assert.Equal(t, http.StatusUnauthorized, resp.StatusCode, "unexpected response code")
+	})
+
+	t.Run("account not found", func(t *testing.T) {
+		m.EXPECT().GetAccountByUserID(gomock.Any(), userID).Return(models.Account{}, errors.New("account not found"))
+
+		req, err := http.NewRequest(http.MethodGet, path, http.NoBody)
+		require.NoError(t, err)
+		ctx := context.WithValue(req.Context(), middleware.AuthenticatedUserKey, userID)
+		req = req.WithContext(ctx)
+
+		r := httptest.NewRecorder()
+
+		h.GetBalance(r, req)
+		resp := r.Result()
+		defer resp.Body.Close()
+
+		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode, "unexpected response code")
 	})
 }
