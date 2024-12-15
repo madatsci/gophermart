7470c5febf0d13b0ec53970053083d0297cb1c57
Andrei Shevkaliuk <sedemaqu@gmail.com>
2024-12-15 16:40:34 +0300

    Removed comments

--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -9,24 +9,20 @@ import (
 )
 
 type Store interface {
-	// Users
 	CreateUser(ctx context.Context, user models.User) (models.User, error)
 	GetUserByLogin(ctx context.Context, login string) (models.User, error)
 
-	// Accounts
 	CreateAccount(ctx context.Context, account models.Account) (models.Account, error)
 	GetAccountByUserID(ctx context.Context, userID string) (models.Account, error)
 	WithdrawBalance(ctx context.Context, userID string, orderNumber string, sum float32) (models.Account, error)
 	AddBalance(ctx context.Context, order models.Order) (models.Account, error)
 
-	// Orders
 	CreateOrder(ctx context.Context, order *models.Order) error
 	GetOrderByNumber(ctx context.Context, orderNumber string) (models.Order, error)
 	ListOrdersByAccountID(ctx context.Context, accountID string, limit int) ([]models.Order, error)
 	ListOrdersByStatus(ctx context.Context, statuses []models.OrderStatus, limit int) ([]models.Order, error)
 	UpdateOrder(ctx context.Context, order models.Order, prevStatus models.OrderStatus) (models.Order, error)
 
-	// Transactions
 	GetWithdrawals(ctx context.Context, accountID string, direction models.TxDirection, limit int) ([]models.Transaction, error)
 }
 
